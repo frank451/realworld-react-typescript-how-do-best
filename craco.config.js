b8ac5814ff705f4dc81403fc34c312bd07fd8747
Frank Uekermann <frank.uekermann@denkwerk.com>
2019-12-15 17:58:53 +0100

    Add files via upload

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+  
+module.exports = {
+    style: {
+        postcss: {
+            env: {
+                stage: 3,
+                features: {
+                    "nesting-rules": true
+                }
+        }
+    }
+}
+}
